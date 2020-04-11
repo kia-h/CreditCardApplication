94928b9d6f9a89f5e2a035d8339880443c790d35
kia-h <46kia-h@users.noreply.github.com>
2020-04-11 15:04:28 +1000

    Add test for high income, low income, young age. also add moq nuget package

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -8,6 +8,32 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void AcceptHighIncomeApplications()
         {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 100_000
+            };
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted,decision);
+
+        }
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication { Age = 19};
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman,decision);
+        }
+
+        [Fact]
+        public void DeclinedLowIncomeApplication()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication {GrossAnnualIncome = 19_000,Age =21};
+            var decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined,decision);
 
         }
     }
