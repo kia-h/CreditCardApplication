9fb45f4702e7613e9300b518fbb35aed32d1dc45
kia-h <46kia-h@users.noreply.github.com>
2020-04-11 14:29:22 +1000

    Add project files.

---
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreditCardApplications.Tests
+{
+    public class CreditCardApplicationEvaluatorShould
+    {
+        [Fact]
+        public void AcceptHighIncomeApplications()
+        {
+
+        }
+    }
+}
